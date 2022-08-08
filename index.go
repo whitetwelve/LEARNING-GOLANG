7288392a7bf294f1a5dbbef16316897907b42cd4
unknown <azkiawardani7@gmail.com>
2022-08-08 23:25:47 +0700

    first

--- index.go
+++ index.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -52,4 +53,34 @@ func main() {
 		c     = a * b
 	)
 	fmt.Println(c)
+	// --PEMBULATAN--//
+	fmt.Println(math.Round(1.5))
+	fmt.Println(math.Round(1.4))
+	fmt.Println(math.Floor(1.7))
+	fmt.Println(math.Ceil(1.3))
+
+	fmt.Println(math.Max(10, 20))
+	fmt.Println(math.Min(10, 20))
+	// PENGKONDISIAN VALUE BOOL //
+	var name1 = "Fuad"
+	var name2 = "Fuad"
+
+	var hasil bool = name1 == name2
+	fmt.Println(hasil)
+
+	var value1 = 100
+	var value2 = 200
+
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+
+	// LOOPING //
+	for i := 0; i <= 10; i++ {
+		if i == 2 {
+			break
+		}
+		fmt.Println("Perulangan ke ", i)
+	}
 }
